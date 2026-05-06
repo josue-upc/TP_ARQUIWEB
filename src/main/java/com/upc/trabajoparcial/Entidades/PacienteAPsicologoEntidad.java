b7330b5a03cf85563d13fe2a201b93a7840a6a7e
JoseRo023 <jchancafe05@gmail.com>
2026-05-06 00:26:19 -0500

    Add PacientesAPsicologoEntidad entity class

---
+++ src/main/java/com/upc/trabajoparcial/Entidades/PacienteAPsicologoEntidad.java
@@ -0,0 +1,33 @@
+package com.upc.trabajoparcial.Entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Table(name = "psychologist_patients")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PacientesAPsicologoEntidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "psicologo_id")
+    private UsuarioEntidad psicologo;
+
+    @ManyToOne
+    @JoinColumn(name = "paciente_id")
+    private UsuarioEntidad paciente;
+
+    @CreationTimestamp
+    private LocalDateTime fechaAsignacion;
+}
