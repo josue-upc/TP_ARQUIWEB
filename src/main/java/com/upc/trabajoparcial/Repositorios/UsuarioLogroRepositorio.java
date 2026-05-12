c897e11e81ef9042dfad3047b001460284abce99
Jaren Quino <jarenquinoaraujo@gmail.com>
2026-05-12 14:59:04 -0500

    feat: correción

--- src/main/java/com/upc/trabajoparcial/Repositorios/UsuarioLogroRepositorio.java
+++ src/main/java/com/upc/trabajoparcial/Repositorios/UsuarioLogroRepositorio.java
@@ -4,6 +4,8 @@ import com.upc.trabajoparcial.Entidades.UsuarioLogroEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioLogroRepositorio extends JpaRepository<UsuarioLogroEntidad, Long> {
       List<UsuarioLogroEntidad> findByUsuarioId(Long usuarioId);
